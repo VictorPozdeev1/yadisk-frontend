1c1676fffbe89828a5b5469ec1f54ae94fb6681d
Slumrag <mrgreenthecristal@yandex.ru>
2023-11-11 19:08:33 +0300

    feat: install material UI lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
-
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Team3!</h1>
+      <CssBaseline>
+        <h1>Hello Team3!</h1>
+        <Button>button</Button>
+      </CssBaseline>
     </div>
   );
 }
