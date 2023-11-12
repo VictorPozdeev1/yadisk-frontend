707b245386bf4143833053983441f2747f5a6592
Gaik Artur <gaik.artur@yandex.ru>
2023-11-12 14:29:53 +0300

    interfaces

--- src/data/contracts/AppState.ts
+++
@@ -1,6 +0,0 @@
-interface AppState {
-    documents: Document[];
-    categories: Category[];
-    selectedCategory: string | null; // Идентификатор выбранной категории
-    selectedDocument: string | null; // Идентификатор выбранного документа
-}
--- src/data/contracts/User.ts
+++
@@ -1,5 +0,0 @@
-interface User {
-    id: string; // Уникальный идентификатор пользователя
-    username: string; // Имя пользователя
-    oauthToken: string; // OAuth токен пользователя для взаимодействия с Яндекс Диском
-}
