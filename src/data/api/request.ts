aef88d2bffdfc51d386d4b15bee1539c5cb833ad
Vladislav Mungin <93925061+VladislavMungin@users.noreply.github.com>
2023-11-17 00:03:40 +0300

    Update request.ts

--- src/data/api/request.ts
+++ src/data/api/request.ts
@@ -5,6 +5,12 @@ import Category from "../contracts/Category";
 
 import token from "./token";
 
+function getCategoryFromPath(path: string): string {
+  const parts = path.split("/");
+  const category = parts[parts.length - 2];
+  return category;
+}
+
 export const CATEGORIES_URL = "CaseLabDocuments",
   BASE_URL = "https://cloud-api.yandex.net/v1/disk/resources";
 
@@ -37,7 +43,7 @@ export const getDocumentsByCategory = async (categories: string) => {
         params: {
           path: `${CATEGORIES_URL}/${categories}`,
           fields:
-            "_embedded.items.name, _embedded.items.resource_id, _embedded.items.file, _embedded.items.preview, _embedded.items.sizes _embedded.path",
+            "_embedded.items.name, _embedded.items.resource_id, _embedded.items.file, _embedded.items.preview, _embedded.items.sizes",
         },
         headers: {
           Authorization: token,
@@ -58,7 +64,7 @@ export const getDocuments = async () => {
       {
         params: {
           fields:
-            "items.name, items.resource_id, items.file, items.preview, items.sizes",
+            "items.name, items.resource_id, items.file, items.preview, items.sizes, items.path",
         },
         headers: {
           Authorization: token,
@@ -66,7 +72,10 @@ export const getDocuments = async () => {
       }
     );
 
-    return response.data.items;
+    return response.data.items.map((el) => ({
+      ...el,
+      category: getCategoryFromPath(el.path),
+    }));
   } catch (err) {
     console.log(err);
   }
@@ -85,49 +94,52 @@ export const deleteDocument = async (path:string) => {
         }
       }
     )
+    getDocuments();
     return response
   } catch(err){
     console.log(err)
   }
 };
 
-export const switchCategory = async (from:string, path:string) =>{
+export const switchCategory = async (from:string, category:string,fileName:string) =>{
   try{
     const response = await axios.post(
       `${BASE_URL}/move`,
       {
         params:{
           from:from, // откуда пример: CaseLabDocuments/Бухгалтерия/Зима.jpg
-          path:path // куда пример: CaseLabDocuments/Университет/Зима.jpg
+          path:`${CATEGORIES_URL}/${category}/${fileName}` // куда пример: CaseLabDocuments/Университет/Зима.jpg
         },
         headers:{
           Authorization: token,
         }
       }
     )
+    getDocuments();
     return response
   } catch(err){
     console.log(err)
   }
-}
+};
 
-export const addDocument = async (url:string, path:string) =>{
-  try{
-    const response = await axios.post(
-      `${BASE_URL}/upload`,
-      {
-        params:{
-          path:path,  // куда будет помещен документ пример: CaseLabDocuments/Бухгалтерия
-          url: url // URL внешнего ресурса, который следует загрузить.
-        },
-        headers:{
-          Authorization: token,
-        }
-      }
-    )
-    return response
-  } catch(err){
-    console.log(err)
-  }
-}
+// export const addDocument = async (url:string, path:string) =>{
+//   try{
+//     const response = await axios.post(
+//       `${BASE_URL}/upload`,
+//       {
+//         params:{
+//           path:path,  // куда будет помещен документ пример: CaseLabDocuments/Бухгалтерия
+//           url: url // URL внешнего ресурса, который следует загрузить.
+//         },
+//         headers:{
+//           Authorization: `${token}`,
+//         }
+        
+//       }
+//     )
+//     return response
+//   } catch(err){
+//     console.log(err)
+//   }
+// }
 
