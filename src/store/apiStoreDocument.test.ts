fa6663c61f661e9a4d386edac6334b13cc02be8b
MVKolodin <144109446+MVKolodin@users.noreply.github.com>
2023-11-16 23:15:02 +0300

    React async tests (#26)
    * Tests for mobx and async requests
    
    * Tests for mobx and async requests

---
+++ src/store/apiStoreDocument.test.ts
@@ -0,0 +1,32 @@
+import ApiStoreCategories from "./apiStoreCategories";
+import { getCategories } from "../data/api/request";
+
+jest.mock("../data/api/request");
+
+describe("ApiStoreCategories", () => {
+    beforeEach(() => {
+        // Сбросить моки перед каждым тестом
+        jest.clearAllMocks();
+    });
+
+    it("should initialize with an empty array of categories", () => {
+        const apiStore = new ApiStoreCategories();
+        expect(apiStore.categories).toEqual([]);
+    });
+
+    it("should load categories and update the state", async () => {
+        const mockCategories = [
+            { name: "Category1", resource_id: "123" },
+            { name: "Category2", resource_id: "456" },
+        ];
+
+        (getCategories as jest.MockedFunction<typeof getCategories>).mockResolvedValue(mockCategories);
+
+        const apiStore = new ApiStoreCategories();
+
+        await apiStore.loadCategories();
+
+        expect(getCategories).toHaveBeenCalled();
+        expect(apiStore.categories).toEqual(mockCategories);
+    });
+});
