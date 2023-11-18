ab8019a5ee891014e0d82fe0ce6a53c9b6e18300
Slumrag <mrgreenthecristal@yandex.ru>
2023-11-18 17:27:40 +0300

    feat: add title & responsive layout to Main

--- src/components/Ui/Main/Main.tsx
+++ src/components/Ui/Main/Main.tsx
@@ -2,19 +2,31 @@ import React, { FC, PropsWithChildren } from 'react';
 import { Outlet } from 'react-router';
 import { DocumentTable } from '../DocumentTable/DocumentTable';
 import Box from '@mui/material/Box';
+import { Grid, Typography, useMediaQuery } from '@mui/material';
+import { Theme } from '@mui/system';
 
 export interface MainProps {
-
+  title?: string;
 }
-export const Main: FC<PropsWithChildren<MainProps>> = () => {
+export const Main: FC<PropsWithChildren<MainProps>> = ({ title }) => {
+  const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up('tablet'));
 
   return (
-    <Box component={'main'} sx={{
-      // display: 'flex',
-      // flexDirection: 'column'
-    }}>
-      {/* <DocumentTable  /> */}
-      <Outlet />
-    </Box>
+    <Grid
+      container
+      component={'main'}
+      direction={'column'}
+      gap={'35px'}
+      p={isDesktop ? '25px 20px' : '0'}
+    >
+      <Grid item>
+        <Typography variant='h2' component='h2'>
+          {title ? title : "Все категории"}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Outlet />
+      </Grid>
+    </Grid>
   )
 }
