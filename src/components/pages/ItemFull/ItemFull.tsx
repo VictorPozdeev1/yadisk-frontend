44a5a9a2bfc4ab323abd553b090fbb3ee49636fa
Victor Pozdeev <victorpozdeev@gmail.com>
2023-11-20 22:32:32 +0400

    Add box size calculation

--- src/components/pages/ItemFull/ItemFull.tsx
+++ src/components/pages/ItemFull/ItemFull.tsx
@@ -1,12 +1,11 @@
-import React, { FC } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { FC, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import { apiStoreDocuments } from "../../../store";
 import { toJS } from "mobx";
 
-import { Dialog, DialogTitle, DialogContent, Typography, Divider } from '@mui/material';
+import { Dialog, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 
 import Spinner from "../../Ui/Spinner/Spinner";
 
@@ -14,72 +13,50 @@ const ItemFull: FC = () => {
   const { id } = useParams();
   const document = toJS(apiStoreDocuments.documents)
     .find(d => d.resource_id === id);
-  if (!document)
-    return <Spinner />;
 
   const fullImageUrl = document
     ?.sizes?.[0]
     ?.url;
 
+  const bookRatio: number = 0.7;
+  const [limitedByWidth, setLimitedByWidth] = React.useState<boolean>(window.innerWidth / window.innerHeight < bookRatio);
+  useEffect(() => {
+    function handleResize() {
+      const newLimitedByWidth = window.innerWidth / window.innerHeight < bookRatio;
+      //if (limitedByWidth != newLimitedByWidth)
+      setLimitedByWidth(newLimitedByWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const percentsToTake = 90;
+  const boxSize: { width: string, height: string } = limitedByWidth
+    ? { width: `${percentsToTake}vw`, height: `${(percentsToTake / bookRatio).toFixed(0)}vw` }
+    : { width: `${(percentsToTake * bookRatio).toFixed(0)}vh`, height: `${percentsToTake}vh` }
+
+  if (!document)
+    return <Spinner />;
+
   return (
-    // <Dialog
-    //   open={true}
-    //   onClose={() => { history.back() }} // eslint-disable-line
-    //   sx={{
-    //     minWidth: '100vw',
-    //     minHeight: '100vh',
-    //     // backgroundColor: 'red',
-    //   }}
-    // >
-    //   <DialogTitle padding={1}>
-    //     <Typography variant="h3">
-    //       {(document as Document).name}
-    //     </Typography>
-    //   </DialogTitle>
-    //   <Divider />
-    //   <DialogContent>
-    //     {fullImageUrl ? (
-    //       <img
-    //         src={fullImageUrl}
-    //         alt="12"
-    //       />
-    //     ) : (
-    //       < Typography variant="h4">
-    //         {'not found :('}
-    //       </Typography>
-    //     )
-    //     }
-    //   </DialogContent>
-    // </Dialog >
     <Dialog
       open={true}
-      // sx={{ backgroundColor: 'red' }}
       onClose={() => { history.back() }} // eslint-disable-line
-
+      PaperProps={{ sx: { m: 0 } }}
     >
       <Box
         sx={{
-          // position: 'absolute' as 'absolute',
-          // top: '50%',
-          // left: '50%',
-          width: '64vh',
-          minWidth: '64vh',
-          height: '90vh',
-          minHeight: '90vh',
-          // backgroundColor: 'blue',
           backgroundImage: `url(${fullImageUrl})`,
           backgroundSize: 'contain',
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
+          ...boxSize
         }}
       >
         {fullImageUrl ? (
           null
-          // <img
-          //   src={fullImageUrl}
-          //   alt="12" width={'100%'} height={'100%'}
-          // //background-repeat: no-repeat;
-          // />
         ) : (
           < Typography variant="h4">
             {'Image url not found :('}
